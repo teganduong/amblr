baf6472a9917e3cdf0e50416a3c5f198c9e2e54a
John Boucherie <jvboucherie@gmail.com>
2016-04-19 18:50:51 -0700

    Add a check for users logged in status

--- client/www/js/controllers/leftNavController.js
+++ client/www/js/controllers/leftNavController.js
@@ -1,6 +1,22 @@
 angular.module('amblr.leftnav', [])
-.controller('LeftMenuNav', function($scope, $ionicSideMenuDelegate) {
+.controller('LeftMenuNav', function($scope, $ionicSideMenuDelegate, $http, $rootScope) {
+
+  $scope.loggedIn = false;
+  
+  $scope.isLoggedIn = function() {
+
+    $http.get('/checklogin')
+      .success(function(data) {
+        console.log(data);
+        $rootScope.loggedIn = data;
+      })
+      .error(function(data) {
+        console.log('error: ' + data);
+      });
+  };
+
   $scope.showMenu = function () {
     $ionicSideMenuDelegate.toggleLeft();
+    $scope.isLoggedIn();
   };
 });
