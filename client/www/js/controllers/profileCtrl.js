66a2cc15ae2bb75c851357e311f8800e9db5819e
Tegan Duong <teganduong@gmail.com>
2016-04-21 18:24:01 -0700

    Implement userProfile as a modal

--- client/www/js/controllers/profileCtrl.js
+++ client/www/js/controllers/profileCtrl.js
@@ -1,17 +1,32 @@
 angular.module('amblr.profile', [])
-  .controller('ProfileCtrl', function($scope, $q) {
-    openFB.api({
-      path: '/me',
-      params: {fields: 'id,name'},
-      success: function(user) {
-        console.log(user);
-        $scope.$apply(function() {
-          $scope.user = user;
+  .controller('ProfileCtrl', function($scope, $rootScope, $ionicModal, Users, Routes) {
+
+    $scope.currentUser = {
+      username: 'tegan',
+      email: 'something@gmail.com'
+    };
+
+    // routes created by user
+    $scope.routes = [];
+
+    // POIs created by user
+    $scope.pois = ['something'];
+
+    $ionicModal.fromTemplateUrl('../../templates/profile.html', {
+      scope: $scope,
+      animation: 'slide-in-up',
+      })
+      .then(function (modal) {
+        $scope.modal = modal;
+      });
+
+      $scope.showUserProfileModal = function () {
+        Routes.getRoutes().then(function (routes) {
+          $scope.routes = routes;
+          $scope.modal.show();
         });
-      },
-      error: function(error) {
-        console.log(error);
-        alert('Facebook error: ' + error.error_description);
+       }
+      $scope.hideUserProfileModal = function () {
+        $scope.modal.hide();
       }
-    });
   });
