40e229bc7f85a699daa91a4eca8098dc17d3bb1c
John Boucherie <jvboucherie@gmail.com>
2016-04-22 20:04:59 -0700

    Find routes based on distance from passed in coords

--- server/controllers/routeController.js
+++ server/controllers/routeController.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Route = require('../models/routeModel.js');
 
 exports.getAllRoutes = function(req, res) {
-
   Route.find({}, function(err, routes) {
     if (err) {
       logger.error('ERROR in getAllPOI: ', err);
@@ -21,4 +20,37 @@ exports.updateRoute = function (req, res) {
     function (err, route) {
       res.json(route);
   });
+};
+
+
+exports.getRoutesNearUser = function(req, res) {
+  console.log('Get Routes Near Me', req.body);
+  Route.find({
+    loc: {
+      $near: {
+        $geometry: {
+          type: "Point", 
+          coordinates: [req.body[0], req.body[1]] 
+        }, 
+        $maxDistance: 3000
+      } 
+    } 
+  }, function(err, routes){
+    console.log(routes);
+    console.log(req.body);
+    res.send(routes);
+  })
+}
+
+exports.getRoute = function(req, res) {
+
+  // Route.find({}, function(err, routes) {
+  //   if (err) {
+  //     logger.error('ERROR in getAllPOI: ', err);
+  //     return res.json(err);
+  //   } 
+      
+  //   // logger.info('Successfully retrieved pois: ' + pois);
+  //   res.json(pois); 
+  // });
 };
